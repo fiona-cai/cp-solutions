ea2e6b033ce092e2f8d40dfdd29aa4dcdcc536cc
Dwin2 <68293203+Dwin2@users.noreply.github.com>
2022-03-14 15:12:33 -0400

    Create j3.cpp

---
+++ ccc/2022/j3.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+#define F first
+#define S second
+#define PB push_back
+const int maxn = 1e5 + 5;
+
+string s;
+
+int main() {
+    cin >> s;
+    int i=0;
+    string currans;
+    while (i < s.size()) {
+        if (s[i] == '+' || s[i] == '-') {
+            cout << currans << (s[i] == '+' ? " tighten" : " loosen") << " ";
+            i ++;
+            while (isdigit(s[i])) {
+                cout << s[i];
+                i++;
+            }
+            cout << "\n";
+            currans = "";
+        } else {
+            currans += s[i];
+            i++;
+        }
+    }
+    return 0 ;
+}
