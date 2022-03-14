fa5f029a8fcf983581afdc9a545581d5ec7e473a
Dwin2 <68293203+Dwin2@users.noreply.github.com>
2022-03-14 15:16:14 -0400

    Create s3.py

---
+++ ccc/2021/s3.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+friends = []
+for _ in range(n):
+    p,w,d = (int(x) for x in input().split())
+    friends.append([p,w,d])
+
+
+def dist(pos):
+    sum = 0
+    for i in range(n):
+        p,w,d = friends[i]
+        if p == pos:
+           continue
+        elif p > pos:
+            p -= d
+            p = max(pos, p)
+            sum += abs(p-pos)*w
+        else:
+            p += d
+            p = min(p,pos)
+            sum += abs(p-pos)*w
+    return sum
+
+lastsum = 999999999999999
+
+low = 0
+high = int(1e9)
+
+while low < high:
+    mid = (low+high)//2
+    curr, curr1 = dist(mid), dist(mid+1)
+
+    if curr > curr1:
+        low = mid+1
+    else:
+        high = mid
+
+print(dist(low))
