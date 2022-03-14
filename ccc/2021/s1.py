934880cbb58894b472fe50da4e283daa678db6a3
Dwin2 <68293203+Dwin2@users.noreply.github.com>
2022-03-14 15:17:23 -0400

    Create s1.py

---
+++ ccc/2021/s1.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+heights = list(map(int, input().split()))
+bases = list(map(int, input().split()))
+
+
+t = 0
+
+for i in range(n):
+    h1 = heights[i]
+    h2 = heights[i+1]
+    b = bases[i]
+
+    a1 = (abs(h1-h2)*b)/2
+    a2 = min(h1, h2) * b
+
+    t += a1 + a2
+
+
+if (t*10) % 10 == 0:
+
+    print(int(t))
+
+else: print(t)
