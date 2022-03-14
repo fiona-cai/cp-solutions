833fb85e3453be986d19faed4e98d23eed2790f9
Dwin2 <68293203+Dwin2@users.noreply.github.com>
2022-03-14 15:18:22 -0400

    Create j2.py

---
+++ ccc/2021/j2.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+bids = []
+
+for i in range(n):
+    name = input()
+    bid = int(input())
+
+    bids.append([bid, name])
+
+my_max = 0
+output = ""
+
+for i in bids:
+    if i[0] > my_max:
+        output = i[1]
+        my_max = i[0]
+
+print(output)
