789d653db72477bf854ba9aa2a77a26e4c92a4ff
Dwin2 <68293203+Dwin2@users.noreply.github.com>
2022-03-14 18:54:43 -0400

    Create s1.py

---
+++ ccc/2020/s1.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+
+m = []
+
+for i in range(n):
+    a, b = (int(x) for x in input().split())
+    m.append([a, b])
+
+m.sort(key=lambda e : e[0])
+
+max = 0
+
+for i in range(len(m)-1):
+    pos2 = m[i+1][1]
+    pos1 = m[i][1]
+    t2 = m[i+1][0]
+    t1 = m[i][0]
+    speed = abs((pos2-pos1)/(t2-t1))
+    if speed > max:
+        max = speed
+
+print(max)
