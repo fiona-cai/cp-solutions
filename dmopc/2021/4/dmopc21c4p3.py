7ce1a71c1cbb880b31858e073d0d85e4310c6595
Fiona Cai <69484076+fiona-cai@users.noreply.github.com>
2022-03-15 10:26:48 -0400

    Create dmopc21c4p3.py

---
+++ dmopc/2021/4/dmopc21c4p3.py
@@ -0,0 +1,13 @@
+#https://dmoj.ca/problem/dmopc21c4p3
+#DMOPC '21 Contest 4 P3 - Palindromic Presents
+
+print("0")
+print("10001")
+print("10101")
+print("10201")
+print("10301")
+print("11011")
+print("11111")
+print("11211")
+print("12021")
+print("13031")
