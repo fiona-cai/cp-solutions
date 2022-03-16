14f520146a4e656be917bf1a8808164eeefc4421
Fiona Cai <69484076+fiona-cai@users.noreply.github.com>
2022-03-15 21:33:51 -0400

    Create j1.py

---
+++ ccc/2010/j1.py
@@ -0,0 +1,25 @@
+#https://dmoj.ca/problem/ccc10j1
+#CCC '10 J1 - What is n, Daddy?
+
+n = int(input())
+
+if n == 1:
+    print("1")
+elif n == 2:
+    print("2")
+elif n == 3:
+    print("2")
+elif n == 4:
+    print("3")
+elif n == 5:
+    print("3")
+elif n == 6:
+    print("3")
+elif n == 7:
+    print("2")
+elif n == 8:
+    print("2")
+elif n == 9:
+    print("1")
+elif n == 10:
+    print("1")
